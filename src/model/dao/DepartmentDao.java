27a1e80e472180d86896c826b01062a685ecac77
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-10-15 15:28:05 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj); // insere um novo departamento
+	void update(Department obj);// atualiza um departamento existent
+	void deletById(Integer id);// remove um departamento existe pelo ID
+	Department findById(Integer id);// detorna um departamento existente pelo ID
+	List<Department> findAll();// retorna todos os departamentos atraves de uma lista
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+
+	void insert(Seller obj); // insere um novo funcionario
+	void update(Seller obj);// atualiza dados de um funcionario existent
+	void deletById(Integer id);// remove um funcionario existe pelo ID
+	Seller findById(Integer id);// detorna um funcionario existente pelo ID
+	List<Seller> findAll();// retorna todos os funcionario atraves de uma lista
+	
+}
