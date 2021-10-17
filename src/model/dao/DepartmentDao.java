0534da15628ec1721dd53a836594686e0ecf9c23
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-10-16 21:17:34 -0300

    DepartmentDaoJDBC and update DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -9,6 +9,6 @@ public interface DepartmentDao {
 	void insert(Department obj); // insere um novo departamento
 	void update(Department obj);// atualiza um departamento existent
 	void deletById(Integer id);// remove um departamento existe pelo ID
-	Department findById(Integer id);// detorna um departamento existente pelo ID
+	Department findById(Integer id);// retorna um departamento existente pelo ID
 	List<Department> findAll();// retorna todos os departamentos atraves de uma lista
 }
